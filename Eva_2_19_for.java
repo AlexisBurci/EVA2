171a946b0cf381b75652a2773a423a79d5b9a649
AlexisBurci <159494505+AlexisBurci@users.noreply.github.com>
2024-04-10 16:50:24 -0600

    Add files via upload

---
+++ Eva_2_19_for.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva_2_19_for;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author invitado
+ */
+public class Eva_2_19_for {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        // TODO code application logic here
+        int numAlum;
+        Scanner captu = new Scanner(System.in);
+        System.out.println("Cuantos estudiantes tiene el grupo");
+        numAlum = captu.nextInt();
+        for(int i = 0; i < 10; i++){
+            System.out.println("calificaciones");
+            int califa = captu.nextInt();
+            
+        }
+    }
+    
+}
